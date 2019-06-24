17abe782c55cce8d4c0f25d59c112e599b31067b
Drew <thesimpledev@gmail.com>
2019-06-24 13:20:22 -0500

    Account for zero on multiplier

--- src/helpers/conversions.js
+++ src/helpers/conversions.js
@@ -1,5 +1,5 @@
 export const oneGallonMultiplier = (units, amount) => {
-  if (amount === "") return 0
+  if (amount === "" || amount == 0) return 0
   const parsedAmount = parseFloat(amount)
   const convertedAmount = (units === "gallons" ? parsedAmount : litersToGallons(parsedAmount))
   return (1 / convertedAmount).toFixed(2)
